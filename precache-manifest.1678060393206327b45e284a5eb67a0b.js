af9013ea15f856dff43f31a96ce7327ee76e6f11
levelasquez <levelasquez@users.noreply.github.com>
2022-04-11 23:17:23 +0000

    deploy: 6264871ea511ee0fe93ac422fe2d6bed9f4f47cd

---
+++ precache-manifest.1678060393206327b45e284a5eb67a0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2255d82f8008477aa5627e324ed59162",
+    "url": "https://levelasquez.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "c3e245bcdeb005b4e3f8",
+    "url": "https://levelasquez.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "139ffd9c89b6c6c0d5d3",
+    "url": "https://levelasquez.github.io/task-management-frontend/static/js/2.801ede08.chunk.js"
+  },
+  {
+    "revision": "c3e245bcdeb005b4e3f8",
+    "url": "https://levelasquez.github.io/task-management-frontend/static/js/main.ee053ed7.chunk.js"
+  },
+  {
+    "revision": "00f69cb39c7e72afc79c",
+    "url": "https://levelasquez.github.io/task-management-frontend/static/js/runtime~main.4cc15314.js"
+  }
+]);
